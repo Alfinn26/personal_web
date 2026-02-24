c67d4059a0ab6117c133cf369c45574b12741267
Alfinn26 <alfinsabil647@gmail.com>
2026-02-24 11:25:20 +0700

    Add mousemove and mouseleave effects to profile card

--- script.js
+++ script.js
@@ -23,6 +23,26 @@ duration:1200,
 once:true
 });
 
+const card = document.querySelector(".card-inner");
+
+document.querySelector(".profile-card").addEventListener("mousemove", (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = -(y - centerY) / 10;
+    const rotateY = (x - centerX) / 10;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+document.querySelector(".profile-card").addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0deg) rotateY(0deg)";
+});
+
 // PARTICLES CONFIG
 particlesJS("particles-js", {
   particles: {
