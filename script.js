3d318f9f83bad4fef20c28cedc3372eb05b2fc30
Alfinn26 <alfinsabil647@gmail.com>
2026-03-11 03:48:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-const card = document.getElementById("profile");
+// smooth scroll animasi sederhana
 
-card.addEventListener("mousemove", (e) => {
-let rect = card.getBoundingClientRect();
-let x = e.clientX - rect.left;
-let y = e.clientY - rect.top;
+document.querySelectorAll("nav a").forEach(link=>{
+link.addEventListener("click",function(e){
 
-let centerX = rect.width / 2;
-let centerY = rect.height / 2;
+e.preventDefault()
 
-let rotateX = -(y - centerY) / 20;
-let rotateY = (x - centerX) / 20;
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+})
 
-card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-card.style.boxShadow = "0 0 40px #00ffff";
-});
-
-card.addEventListener("mouseleave", () => {
-card.style.transform = "rotateX(0) rotateY(0)";
-card.style.boxShadow = "0 0 20px #00ffff";
-});
+})
+})
